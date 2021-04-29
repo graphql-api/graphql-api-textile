cd3adf2d29a5a810eb8d636b051420902fd99038
jfa <git@jonathanallenberg.de>
2021-04-29 17:36:03 +0200

    …

---
+++ src/lib/collectUint8Arrays.ts
@@ -0,0 +1,19 @@
+export async function collectUint8Arrays(it: AsyncIterable<Uint8Array>): Promise<Uint8Array> {
+  const chunks = []
+  let length = 0
+  for await (const chunk of it) {
+    chunks.push(chunk)
+    length += chunk.length
+  }
+  if (chunks.length === 1) {
+    // No need to copy.
+    return chunks[0]
+  }
+  const collected = new Uint8Array(length)
+  let offset = 0
+  for (let chunk of chunks) {
+    collected.set(chunk, offset)
+    offset += chunk.length
+  }
+  return collected
+}
